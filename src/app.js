10285056181b2f4e3edb2e0887f108494de5b52a
Nicole Caceres <janenieggcs@gmail.com>
2022-09-15 12:21:08 -0500

    Second commit - App Express - Texto agregado en ruta raíz

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import { cliente } from "./components"
 import { producto } from "./components"
 
 export const app = express();
-
+app.get('/', (req,res)=>{
+    res.send('Hola mundo, app con Express. De: Nicole Jane Cáceres Serna')
+})
 //nuestro servidor estara autorizado 
 app.use(cors())
 
